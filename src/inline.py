2c2229ec78cbdd7ab3682df4800d11164a0d6931
Nongrenost <Nongrenost@gmail.com>
2025-04-25 20:43:05 +0300

    split nodes image

--- src/inline.py
+++ src/inline.py
@@ -1,8 +1,9 @@
 from src.textnode import TextType, TextNode
 from typing import List
+import re
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> List[TextNode]:
-    """split 'text' textnodes by delimiter into textnodes with correct text type"""
+    """split 'text' textnodes by delimiter into a list of textnodes with correct text type"""
 
     result = []
     # iterate over input list
@@ -29,3 +30,48 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                 
     return result
 
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    """return a list of (alt text, image_url) pairs from markdown string"""
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    result = re.findall(pattern, text)
+    return result
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    """return a list of (alt text, link_url) pairs from markdown string"""
+    pattern = "(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"   
+    result = re.findall(pattern, text)
+    return result
+
+def split_nodes_image(old_nodes: List[TextNode]) -> List[TextNode]:
+    """given list of textnodes, split its text into list of text and image textnodes"""
+       
+    result = []
+    for old_node in old_nodes:
+        text = old_node.text
+        while text:
+            images = extract_markdown_images(text)
+            
+            if not extract_markdown_images(text):
+                result.append(TextNode(text,TextType.TEXT))
+                break
+            
+            delimiter = f"![{images[0][0]}]({images[0][1]})"
+            splitted = text.split(delimiter, 1)
+            chunk = ""
+            
+            if splitted[0] == "":
+                result.append(TextNode(images[0][0], TextType.IMAGE, images[0][1]))
+                chunk = delimiter
+            else:
+                result.append(TextNode(splitted[0], TextType.TEXT))
+                chunk = splitted[0]
+            text = text.lstrip(chunk)
+    return result
+        
+def split_nodes_link(old_nodes: List[TextNode]) -> List[TextNode]:
+    """given list of textnodes, split its text into list of text and link textnodes"""
+    return [TextNode("text", TextType.TEXT)]
+
+# def splitter(old_node: List[TextNode]) -> list[TextNode]:
+#     return 
+    
--- src/tests/test_inline.py
+++ src/tests/test_inline.py
@@ -1,6 +1,7 @@
 import pytest
-from src.inline import split_nodes_delimiter
+from src.inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from src.textnode import TextNode, TextType
+
 
 
 
@@ -76,3 +77,131 @@ def test_split_node_codeblock_with_other_delimiters() -> None:
     result = [TextNode('hello_string = "Hello, World!"', TextType.CODE)]
     
     assert new_nodes == result
+
+def test_extract_markdown_images() -> None:
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    extracted = extract_markdown_images(text)
+    result = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+    assert extracted == result
+    
+
+def test_extract_markdown_links() -> None:
+    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    extracted = extract_markdown_links(text)
+    result = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+    assert extracted == result
+    
+def test_split_nodes_link() -> None:
+    node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT)
+    splitted = split_nodes_link([node])
+    result = [
+     TextNode("This is text with a link ", TextType.TEXT),
+     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+     TextNode(" and ", TextType.TEXT),
+     TextNode(
+         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+     )]
+    assert splitted == result
+    
+    
+def test_split_nodes_image6() -> None:
+    node = TextNode(
+        "TEXT1",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("TEXT1", TextType.TEXT),
+            ]
+    assert splitted == result
+    
+    
+def test_split_nodes_image7() -> None:
+    node = TextNode(
+        "![IMAGE1](https://i.imgur.com/zjjcJKZ.png)",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("IMAGE1", TextType.IMAGE,"https://i.imgur.com/zjjcJKZ.png"),
+            ]
+    assert splitted == result
+
+def test_split_nodes_image8() -> None:
+    node = TextNode(
+        "TEXT1 ![IMAGE1](https://i.imgur.com/zjjcJKZ.png)",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("TEXT1 ", TextType.TEXT),
+            TextNode("IMAGE1", TextType.IMAGE,"https://i.imgur.com/zjjcJKZ.png"),
+            ]
+    assert splitted == result
+    
+def test_split_nodes_image() -> None:
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png")
+            ]
+    assert splitted == result
+    
+def test_split_nodes_image2() -> None:
+    node = TextNode(
+        "![IMAGE1](https://i.imgur.com/zjjcJKZ.png) ![IMAGE2](https://i.imgur.com/3elNhQu.png) TEXT1",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("IMAGE1", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("IMAGE2", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            TextNode(" TEXT1", TextType.TEXT)
+            ]
+    assert splitted == result
+
+def test_split_nodes_image3() -> None:
+    node = TextNode(
+        "TEXT1 ![IMAGE1](https://i.imgur.com/zjjcJKZ.png) ![IMAGE2](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("TEXT1 ", TextType.TEXT),
+            TextNode("IMAGE1", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("IMAGE2", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ]
+    assert splitted == result
+
+def test_split_nodes_image4() -> None:
+    node = TextNode(
+        "TEXT1 ![IMAGE1](https://i.imgur.com/zjjcJKZ.png) TEXT2 ![IMAGE1](https://i.imgur.com/zjjcJKZ.png)",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("TEXT1 ", TextType.TEXT),
+            TextNode("IMAGE1", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" TEXT2 ", TextType.TEXT),
+            TextNode("IMAGE1", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ]
+    assert splitted == result
+    
+def test_split_nodes_image5() -> None:
+    node = TextNode(
+        "TEXT1 ![IMAGE1](https://i.imgur.com/zjjcJKZ.png) ![IMAGE2](https://i.imgur.com/3elNhQu.png) ![IMAGE1](https://i.imgur.com/zjjcJKZ.png)",
+        TextType.TEXT)
+    splitted = split_nodes_image([node])
+    result = [
+            TextNode("TEXT1 ", TextType.TEXT),
+            TextNode("IMAGE1", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("IMAGE2", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("IMAGE1", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ]
+    assert splitted == result
+
